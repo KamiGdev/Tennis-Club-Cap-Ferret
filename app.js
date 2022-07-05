b663d10d2ed59458b4a8ee15ef03287e2737163f
Kamilia Gourbail <kami.gourbail@gmail.com>
2022-07-05 17:37:51 +0200

    cookie consent responsive

--- app.js
+++ app.js
@@ -51,3 +51,27 @@ window.addEventListener('scroll', () => {
   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
   })
+
+
+
+
+
+  
+
+
+// Cookie consent sur home page
+
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
